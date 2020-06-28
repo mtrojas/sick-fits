84ad8b96c885a83de058a4c0d5513719036dc038
MT <mtrojas@protonmail.com>
2020-06-28 17:59:50 -0400

    feat(sell): add image updolad functionality with cloudinary.

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -39,6 +39,25 @@ class CreateItem extends Component {
     const val = type === 'number' ? parseFloat(value) : value;
     this.setState({ [name]: val });
   };
+  uploadFile = async e => {
+    console.log('Uploading file...');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch
+    ('https://api.cloudinary.com/v1_1/de3dczgcz/image/upload', {
+      method: 'POST',
+      body: data
+    });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
   render() {
     return (
       <Mutation
@@ -60,41 +79,52 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && <img width="200" src={this.state.image} alt="Upload Preview" />}
+              </label>
               <label htmlFor="title">
                 Title
-              <input
-                type="text"
-                id="title"
-                name="title"
-                placeholder="Title"
-                required
-                value={this.state.title}
-                onChange={this.handleChange}
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
                 />
               </label>
               <label htmlFor="price">
                 Price
-              <input
-                type="number"
-                id="price"
-                name="price"
-                placeholder="Price"
-                required
-                value={this.state.price}
-                onChange={this.handleChange}
-              />
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
               </label>
               <label htmlFor="description">
                 Description
-              <textarea
-                type="text"
-                id="description"
-                name="description"
-                placeholder="Enter A Description"
-                required
-                value={this.state.description}
-                onChange={this.handleChange}
-              />
+                <textarea
+                  type="text"
+                  id="description"
+                  name="description"
+                  placeholder="Enter A Description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
               </label>
               <button type="submit">Submit</button>
             </fieldset>
