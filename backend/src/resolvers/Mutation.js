e4c55588420d439743254ec41ae4d6f7ae3d7c4d
MT <mtrojas@protonmail.com>
2020-06-29 17:21:18 -0400

    feat(update): create updateItem mutation and retrieve one item query, adding it to the graphql schema.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -9,14 +9,21 @@ const Mutations = {
     }, info);
 
     return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    // First take a copy of the updates
+    const updates = { ...args };
+    // Remove the ID from the updates
+    delete updates.id;
+    // Run the update method
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info);
   }
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || [];
-  //   //create a dog!
-  //   const newDog = { name: args.name};
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  // }
 };
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,7 +3,8 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
 // for this query we don't need authentication so we can direct it to our db right away if the query is exactly the same as the one in prisma with no filtering or authentication or custom logic, only pulling data from the db
-  items: forwardTo('db')
+  items: forwardTo('db'),
+  item: forwardTo('db')
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
