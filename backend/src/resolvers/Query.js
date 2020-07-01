7a652e46ec96adbd8b49d7db9797c3bd0152c19e
MT <mtrojas@protonmail.com>
2020-07-01 16:19:08 -0400

    feat(itemsconnection): create itemsconnection query resolver and add it to the schema.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,7 +4,8 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
 // for this query we don't need authentication so we can direct it to our db right away if the query is exactly the same as the one in prisma with no filtering or authentication or custom logic, only pulling data from the db
   items: forwardTo('db'),
-  item: forwardTo('db')
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db')
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
