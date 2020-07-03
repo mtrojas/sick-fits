c57958b1a5010a4c509873f127fd9ff27f1652a8
MT <mtrojas@protonmail.com>
2020-07-02 22:02:09 -0400

    feat(loggedin): implement logic to get logged in userId from jwt. add query me and its resolver.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -5,12 +5,16 @@ const Query = {
 // for this query we don't need authentication so we can direct it to our db right away if the query is exactly the same as the one in prisma with no filtering or authentication or custom logic, only pulling data from the db
   items: forwardTo('db'),
   item: forwardTo('db'),
-  itemsConnection: forwardTo('db')
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // }
-
+  itemsConnection: forwardTo('db'),
+  me(parent, args, context, info) {
+    // Check if there is a current user ID
+    if(!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user({
+      where: { id: ctx.request.userId }
+    }, info);
+  }
 };
 
 module.exports = Query;
