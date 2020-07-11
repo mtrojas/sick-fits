3220c937af8acb4391387ac206de9e9d5b6a9d11
MT <mtrojas@protonmail.com>
2020-07-11 12:24:59 -0400

    fix(delete): check if CartItem exists, if not render a message to the user that it has been removed.

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -21,7 +21,16 @@ const CartItemStyles = styled.li`
   }
 `;
 
-const CartItem = ({ cartItem }) => (
+const CartItem = ({ cartItem }) => {
+  // First check if that item exists
+  if (!cartItem.item)
+    return (
+    <CartItemStyles>
+      <p>This Item has been removed</p>
+      <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+    );
+  return (
   <CartItemStyles>
     <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
     <div className="cart-item-details">
@@ -37,7 +46,7 @@ const CartItem = ({ cartItem }) => (
     </div>
     <RemoveFromCart id={cartItem.id} />
   </CartItemStyles>
-);
+)};
 
 CartItem.propTypes = {
   cartItem: PropTypes.object.isRequired
