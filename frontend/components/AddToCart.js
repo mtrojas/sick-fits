bde492369e1e5334f10efb4ad22966ec735b1d6a
MT <mtrojas@protonmail.com>
2020-07-25 21:31:25 -0400

    test(last): add tests for AddToCart, Cart, Order, RemoFromCart, Signup and TakeMyMoneyComponents.

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -26,7 +26,9 @@ class AddToCart extends React.Component {
         refetchQueries={[{ query: CURRENT_USER_QUERY }]}
       >
         {(addToCart, { loading }) => (
-          <button disabled={loading} onClick={addToCart}>Add{loading && 'ing'} To Cart 🛒</button>
+          <button disabled={loading} onClick={addToCart}>
+            Add{loading && 'ing'} To Cart 🛒
+          </button>
         )}
       </Mutation>
     );
@@ -34,4 +36,5 @@ class AddToCart extends React.Component {
 }
 
 export default AddToCart;
+export { ADD_TO_CART_MUTATION };
 
